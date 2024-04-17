58693ef8bb01163704e15f1b5e70d819b4e178f7
robokrishan <haritkrishan176@gmail.com>
2024-04-17 20:24:27 +0200

    fixed main file

--- main/main.c
+++ main/main.c
@@ -19,52 +19,46 @@
 static const char* TAG = "app_main";
 
 
+// void ultrasonic_test(void *pvParameters)
+// {
+//     ultrasonic_sensor_t* pSensor;
+//     pSensor = pvParameters;
 
+//     ultrasonic_init(pSensor);
 
+//     while (true)
+//     {
+//         float distance;
+//         esp_err_t res = ultrasonic_measure(pSensor, CONFIG_MAX_DISTANCE_CM, &distance);
+//         if (res != ESP_OK) {
+//             printf("Error %d: ", res);
+//             switch (res)
+//             {
+//                 case ESP_ERR_ULTRASONIC_PING:
+//                     // printf("Cannot ping (device is in invalid state)\n");
+//                     printf("-1\n");
+//                     break;
+//                 case ESP_ERR_ULTRASONIC_PING_TIMEOUT:
+//                     // printf("Ping timeout (no device found)\n");
+//                     printf("-2\n");
+//                     break;
+//                 case ESP_ERR_ULTRASONIC_ECHO_TIMEOUT:
+//                     // printf("Echo timeout (i.e. distance too big)\n");
+//                     printf("-3\n");
+//                     break;
+//                 default:
+//                     printf("%s\n", esp_err_to_name(res));
+//             }
+//         }
+//         else {
+//             distance_cm = (uint8_t)(distance*100);
+//             // printf("Distance: %d cm\n", distance_cm);
+//             printf("%d\n", distance_cm);
+//         }
 
-
-
-
-void ultrasonic_test(void *pvParameters)
-{
-    ultrasonic_sensor_t* pSensor;
-    pSensor = pvParameters;
-
-    ultrasonic_init(pSensor);
-
-    while (true)
-    {
-        float distance;
-        esp_err_t res = ultrasonic_measure(pSensor, CONFIG_MAX_DISTANCE_CM, &distance);
-        if (res != ESP_OK) {
-            printf("Error %d: ", res);
-            switch (res)
-            {
-                case ESP_ERR_ULTRASONIC_PING:
-                    // printf("Cannot ping (device is in invalid state)\n");
-                    printf("-1\n");
-                    break;
-                case ESP_ERR_ULTRASONIC_PING_TIMEOUT:
-                    // printf("Ping timeout (no device found)\n");
-                    printf("-2\n");
-                    break;
-                case ESP_ERR_ULTRASONIC_ECHO_TIMEOUT:
-                    // printf("Echo timeout (i.e. distance too big)\n");
-                    printf("-3\n");
-                    break;
-                default:
-                    printf("%s\n", esp_err_to_name(res));
-            }
-        }
-        else {
-            distance_cm = (uint8_t)(distance*100);
-            // printf("Distance: %d cm\n", distance_cm);
-            printf("%d\n", distance_cm);
-        }
-
-        vTaskDelay(pdMS_TO_TICKS(100));
-    }
-}
+//         vTaskDelay(pdMS_TO_TICKS(100));
+//     }
+// }
 
 static void s_checkFirmwareAndPartition(void)
 {
@@ -80,28 +74,13 @@ void app_main(void)
 {
     s_checkFirmwareAndPartition();
 
-    ultrasonic_sensor_t sLeft = {
-        .echo_pin = CONFIG_PIN_SENSOR_LEFT_ECHO,
-        .trigger_pin = CONFIG_PIN_SENSOR_LEFT_TRIGGER
-    };
+    esp_err_t lErr = ESP_OK;
 
-    ultrasonic_sensor_t sFront = {
-        .echo_pin = CONFIG_PIN_SENSOR_FRONT_ECHO,
-        .trigger_pin = CONFIG_PIN_SENSOR_FRONT_TRIGGER
-    };
-
-    ultrasonic_sensor_t sRight = {
-        .echo_pin = CONFIG_PIN_SENSOR_RIGHT_ECHO,
-        .trigger_pin = CONFIG_PIN_SENSOR_RIGHT_TRIGGER
-    };
-
-    xTaskCreatePinnedToCore(ultrasonic_test, 
-                            "bumper_front", 
-                            configMINIMAL_STACK_SIZE * 3, 
-                            &sFront, 
-                            5, 
-                            NULL, 
-                            1);
-
+    lErr = bumperInit();
+    if(lErr) {
+        ESP_LOGE(TAG, "Failed to initialize bumper. Code: 0x%X", lErr);
+    } else {
+        ESP_LOGI(TAG, "Bumper initialized!");
+    }
     
 }
