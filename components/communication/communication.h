3ea50e7b381ba7a51d409a5f5fb55ec800ac01c3
robokrishan <haritkrishan176@gmail.com>
2024-04-18 00:27:52 +0200

    added packet config

--- components/communication/communication.h
+++ components/communication/communication.h
@@ -3,6 +3,9 @@
 
 #include "esp_err.h"
 
+#define BUFFER_SIZE                 (1024)
+#define COMM_PACKET_HEADER          (0xAB3412FE)
+#define COMM_PACKET_TERMINATOR      (0x0A0D)
 
 esp_err_t communicationWrite(uint8_t* pData, size_t ulLen);
 
