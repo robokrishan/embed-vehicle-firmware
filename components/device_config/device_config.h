3c82f72e3e30811e3f2a5012720bb8ef94affe44
robokrishan <haritkrishan176@gmail.com>
2024-04-23 21:25:59 +0200

    bumped version

--- components/device_config/device_config.h
+++ components/device_config/device_config.h
@@ -1,16 +1,16 @@
 #ifndef __DEVICE_CONFIG_H__
 #define __DEVICE_CONFIG_H__
 
-#define CONFIG_DEVICE_FIRMWARE_REV          "1.0.0"
+#define CONFIG_DEVICE_FIRMWARE_REV          "1.1.0"
 
 #define CONFIG_MAX_DISTANCE_CM              200 // 5m max
 
-#define CONFIG_PIN_SENSOR_LEFT_ECHO         4
-#define CONFIG_PIN_SENSOR_LEFT_TRIGGER      5
+#define CONFIG_PIN_SENSOR_LEFT_ECHO         3
+#define CONFIG_PIN_SENSOR_LEFT_TRIGGER      8
 #define CONFIG_PIN_SENSOR_FRONT_ECHO        6
 #define CONFIG_PIN_SENSOR_FRONT_TRIGGER     7
-#define CONFIG_PIN_SENSOR_RIGHT_ECHO        3
-#define CONFIG_PIN_SENSOR_RIGHT_TRIGGER     8
+#define CONFIG_PIN_SENSOR_RIGHT_ECHO        4
+#define CONFIG_PIN_SENSOR_RIGHT_TRIGGER     5
 
 #define CONFIG_PIN_UART_RX1                 18
 #define CONFIG_PIN_UART_TX1                 17
